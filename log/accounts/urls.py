964bd98dc2fb2c1018b8fb6a6e5e4b22e82687f8
laxmaniron <laxmankumarapu@gmail.com>
2018-11-12 00:01:00 +0530

    password resetting part is done

--- log/accounts/urls.py
+++ log/accounts/urls.py
@@ -11,13 +11,18 @@ urlpatterns=[
         path('profile/edit/',views.edit_profile,name='edit_profile'),
         path('profile/password/',views.change_password,name='change_password'),
         path('change-password/',views.change_password,name='change_password'), #change password url
-        path('reset-password/',auth_views.PasswordResetView.as_view(),name='password_reset'), # password reset url
+
+        path('reset-password/',auth_views.PasswordResetView.as_view(template_name='accounts/reset_password.html'
+        ,email_template_name = 'accounts/reset_password_email.html'),name='password_reset'), # password reset url
         # password reset done url
-        path('reset-password/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+        path('reset-password/done/',auth_views.PasswordResetDoneView.as_view(template_name='accounts/reset_password_done.html'),
+        name='password_reset_done'),
         #password reset confirm url
-        path('reset-password/confirm/(<uidb64>[0-9A-Za-z]+)-(<token>.+)/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+        path('reset-password/confirm/(<uidb64>[0-9A-Za-z]+)-(<token>.+)/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/reset_password_confirm.html'),
+        name='password_reset_confirm'),
         #password reset complete url
-        path('reset-password/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-        path('reset/real', include('django.contrib.auth.urls')),
+        path('reset-password/complete/',auth_views.PasswordResetCompleteView.as_view(template_name='accounts/reset_password_complete.html'),name='password_reset_complete'),
+        #path('reset/real', include('django.contrib.auth.urls')),
 
 ]
