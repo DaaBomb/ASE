23e52d10644a9858b9a3001c8d4910be39964de8
nischal <nischal.t17@iiits.in>
2018-11-13 18:03:10 +0530

    comments can be viewed by admin users,post detail is done

--- mpart/blog/views.py
+++ mpart/blog/views.py
@@ -1,7 +1,13 @@
+import post as post
+from django.utils import timezone
 from django.shortcuts import render
 from blog.models import post
 # Create your views here.
 
 def index(request):
-    posts_dict={'posts':post.objects.all()}
+    posts_dict={'posts':post.objects.order_by('-timestamp')}
     return render(request, 'blog/index.html', context=posts_dict)
+
+def details(request, pk):
+    posts_dict={'posts':post.objects.filter(id=pk)}
+    return render(request, 'blog/details.html', context=posts_dict)
